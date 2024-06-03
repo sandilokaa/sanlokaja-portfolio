538ac099c7c9815ea65c0c65b6bc2bc81604c381
sandilokaa <sandyloka790@gmail.com>
2024-06-03 16:01:27 +0700

    docs: add jumbotron

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -21,6 +21,7 @@ import LetsTalkIcon from "../../assets/images/lets-talk.svg";
 import MyBestProjectIcon from "../../assets/images/best-project.svg";
 import ArrowIcon from "../../assets/images/arrow.svg";
 import ArrowStabIcon from "../../assets/images/arrow-stab.svg";
+import JumbotronImage from "../../assets/images/jumbotron.png";
 
 
 import "../../assets/css/style.css";
@@ -38,6 +39,21 @@ const Home = () => {
 
     const showServiceDetail = (serviceName) => {
         setSelectedServiceButton(serviceName);
+    };
+
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseMove = (event) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+        setMousePosition({ x: mouseX, y: mouseY });
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
     };
 
     return (
@@ -111,7 +127,7 @@ const Home = () => {
                             <Col xs={12} xl={12}>
                                 <div className="header-round-arrow">
                                     <div className="header-round-arrow-inside">
-                                        <Image src={ArrowStabIcon}/>
+                                        <Image src={ArrowStabIcon} />
                                     </div>
                                 </div>
                                 <Image src={HeaderBigImage} />
@@ -169,7 +185,7 @@ const Home = () => {
                         <Col xs={12} xl={6} className="my-service-button">
                             <Row>
                                 <Col xs={6} xl={6} className="d-flex justify-content-end">
-                                    <Button 
+                                    <Button
                                         onClick={() => showServiceDetail('Fullstack Dev')}
                                         className={selectedServiceButton === 'Fullstack Dev' ? 'active' : ''}
                                     >
@@ -177,7 +193,7 @@ const Home = () => {
                                     </Button>
                                 </Col>
                                 <Col xs={6} xl={6} className="d-flex justify-content-end">
-                                    <Button 
+                                    <Button
                                         onClick={() => showServiceDetail('Frontend Dev')}
                                         className={selectedServiceButton === 'Frontend Dev' ? 'active' : ''}
                                     >
@@ -187,7 +203,7 @@ const Home = () => {
                             </Row>
                             <Row>
                                 <Col xs={6} xl={6} className="d-flex justify-content-end">
-                                    <Button 
+                                    <Button
                                         onClick={() => showServiceDetail('Backend Dev')}
                                         className={selectedServiceButton === 'Backend Dev' ? 'active' : ''}
                                     >
@@ -195,7 +211,7 @@ const Home = () => {
                                     </Button>
                                 </Col>
                                 <Col xs={6} xl={6} className="d-flex justify-content-end">
-                                    <Button 
+                                    <Button
                                         onClick={() => showServiceDetail('Website Design')}
                                         className={selectedServiceButton === 'Website Design' ? 'active' : ''}
                                     >
@@ -222,18 +238,61 @@ const Home = () => {
                         {selectedServiceButton && (
                             <Col xs={12} xl={{ span: 8, offset: 1 }}>
                                 {selectedServiceButton === 'Fullstack Dev' && (
-                                    <FullstackDevService/>
+                                    <FullstackDevService />
                                 )}
                                 {selectedServiceButton === 'Frontend Dev' && (
-                                    <FrontendDevService/>
+                                    <FrontendDevService />
                                 )}
                                 {selectedServiceButton === 'Backend Dev' && (
-                                    <BackendDevService/>
+                                    <BackendDevService />
                                 )}
                                 {selectedServiceButton === 'Website Design' && (
-                                    <WebsiteDesignService/>
+                                    <WebsiteDesignService />
                                 )}
                             </Col>
+                        )}
+                        <hr />
+                    </Row>
+                </Container>
+            </div>
+
+            <div id="jumbotron-portfolio">
+                <Container>
+                    <Row
+                        onMouseMove={handleMouseMove}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <Col xs={12} xl={12}>
+                            <Image src={JumbotronImage} style={{ width: '100%', height: 'auto' }} />
+                        </Col>
+                        {isHovered && (
+                            <div
+                                className="mouse-follower"
+                                style={{
+                                    backgroundColor: '#D5FF3F',
+                                    position: 'absolute',
+                                    left: mousePosition.x - 180 / 2,
+                                    top: mousePosition.y - 180 / 2,
+                                    width: '180px',
+                                    height: '180px',
+                                    borderRadius: '50%',
+                                    pointerEvents: 'none',
+                                    zIndex: '0',
+                                    animation: 'grow 0.4s forwards',
+                                    transition: 'left 0.1s, top 0.1s'
+                                }}
+                            >
+                                <Row style={{ fontWeight: '600', marginTop: '20%', marginLeft: '60%'}}>
+                                    <Col xs={12} xl={12}>
+                                        <Image src={ArrowIcon}/>
+                                    </Col>
+                                </Row>
+                                <Row style={{ fontWeight: '600', marginTop: '40%', marginLeft: '5%', fontSize: '18px' }}>
+                                    <Col xs={12} xl={12}>
+                                <p>My Github</p>
+                                    </Col>
+                                </Row>
+                            </div>
                         )}
                     </Row>
                 </Container>
