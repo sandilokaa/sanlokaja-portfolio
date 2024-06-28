37234126e3a2a20eb46912220c4987c192aa8e97
sandilokaa <sandyloka790@gmail.com>
2024-06-29 01:27:42 +0700

    docs: update best project

---
+++ src/assets/js/projectData.js
@@ -0,0 +1,155 @@
+import Image1 from "../images/motivation-1.png";
+
+export const ProjectData = {
+    "type": "Project",
+    "Collections": [
+        {
+            type: "Fullstack Dev",
+            id: "1",
+            properties:
+            [
+                {
+                    id: 1,
+                    image: Image1,
+                    title: "Warehouse Project",
+                    year: 2023
+                },
+                {
+                    id: 2,
+                    image: Image1,
+                    title: "YukPrivate",
+                    year: 2022
+                },
+                {
+                    id: 3,
+                    image: Image1,
+                    title: "Start Business",
+                    year: 2023
+                },
+                {
+                    id: 4,
+                    image: Image1,
+                    title: "Geopardy",
+                    year: 2024
+                },
+                {
+                    id: 5,
+                    image: Image1,
+                    title: "Other",
+                    year: "2022-Present"
+                }
+            ]
+        },
+        {
+            type: "BackEnd Dev",
+            id: "2",
+            properties:
+            [
+                {
+                    id: 1,
+                    image: Image1,
+                    title: "BudgetIn",
+                    year: 2023
+                },
+                {
+                    id: 2,
+                    image: Image1,
+                    title: "BWA Startup",
+                    year: 2024
+                },
+                {
+                    id: 3,
+                    image: Image1,
+                    title: "Simpin Kopertas",
+                    year: 2023
+                },
+                {
+                    id: 4,
+                    image: Image1,
+                    title: "Skilvul",
+                    year: 2022
+                },
+                {
+                    id: 5,
+                    image: Image1,
+                    title: "Other",
+                    year: "2022-Present"
+                }
+            ]
+        },
+        {
+            type: "FrontEnd Dev",
+            id: "3",
+            properties:
+            [
+                {
+                    id: 1,
+                    image: Image1,
+                    title: "Atsiraessence",
+                    year: 2023
+                },
+                {
+                    id: 2,
+                    image: Image1,
+                    title: "Catalyzon",
+                    year: 2023
+                },
+                {
+                    id: 3,
+                    image: Image1,
+                    title: "Sisrella Corndog",
+                    year: 2022
+                },
+                {
+                    id: 4,
+                    image: Image1,
+                    title: "Full House",
+                    year: 2022
+                },
+                {
+                    id: 5,
+                    image: Image1,
+                    title: "Other",
+                    year: "2022-Present"
+                }
+            ]
+        },
+        {
+            type: "UI UX Design",
+            id: "4",
+            properties:
+            [
+                {
+                    id: 1,
+                    image: Image1,
+                    title: "Warehouse Project",
+                    year: 2023
+                },
+                {
+                    id: 2,
+                    image: Image1,
+                    title: "Catalyzon",
+                    year: 2023
+                },
+                {
+                    id: 3,
+                    image: Image1,
+                    title: "Start Business",
+                    year: 2023
+                },
+                {
+                    id: 4,
+                    image: Image1,
+                    title: "Geopardy",
+                    year: 2024
+                },
+                {
+                    id: 5,
+                    image: Image1,
+                    title: "Other",
+                    year: "2022-Present"
+                }
+            ]
+        }
+    ]
+};
--- src/components/portfolio/BestProject.jsx
+++ src/components/portfolio/BestProject.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Row,
     Col,
     Container,
-    Button
+    Button,
+    Image
 } from "react-bootstrap";
+
+import ArrowExample from "../../assets/images/arrow-project.png";
+
+import { ProjectData } from "../../assets/js/projectData";
 
 import "../../assets/css/style.css";
 import "../../assets/css/responsive.css";
 
 const BestProject = () => {
+
+    /* ---------- Selected Project Button ----------*/
+
+    const [selectedProjectButton, setSelectedProjectButton] = useState(null);
+
+    useEffect(() => {
+        if (selectedProjectButton === null) {
+            setSelectedProjectButton('Fullstack Dev');
+        }
+    }, [selectedProjectButton]);
+
+    const showProjectDetail = (projectName) => {
+        setSelectedProjectButton(projectName);
+    };
+
+    /* ---------- End Selected Project Button ----------*/
+
+    const filteredProjects = ProjectData.Collections.filter(project => project.type === selectedProjectButton);
 
     return (
 
@@ -24,22 +47,34 @@ const BestProject = () => {
             </Row>
             <Row>
                 <Col xs={6} xl={2}>
-                    <Button className="w-100">
+                    <Button
+                        onClick={() => showProjectDetail('Fullstack Dev')}
+                        className={selectedProjectButton === 'Fullstack Dev' ? 'active' : ''}
+                    >
                         Fullstack Dev
                     </Button>
                 </Col>
                 <Col xs={6} xl={2}>
-                    <Button className="w-100">
+                    <Button
+                        onClick={() => showProjectDetail('BackEnd Dev')}
+                        className={selectedProjectButton === 'BackEnd Dev' ? 'active' : ''}
+                    >
                         BackEnd Dev
                     </Button>
                 </Col>
                 <Col xs={6} xl={2}>
-                    <Button className="w-100">
+                    <Button
+                        onClick={() => showProjectDetail('FrontEnd Dev')}
+                        className={selectedProjectButton === 'FrontEnd Dev' ? 'active' : ''}
+                    >
                         FrontEnd Dev
                     </Button>
                 </Col>
                 <Col xs={6} xl={2}>
-                    <Button className="w-100">
+                    <Button
+                        onClick={() => showProjectDetail('UI UX Design')}
+                        className={selectedProjectButton === 'UI UX Design' ? 'active' : ''}
+                    >
                         UI UX Design
                     </Button>
                 </Col>
@@ -49,23 +84,35 @@ const BestProject = () => {
                     <div className="round-my-best-project"></div>
                 </Col>
             </Row>
-            <Row>
-                <Col xs={12} xl={4} className="mt-4">
-                    <div style={{backgroundColor: '#292929', height: '320px'}}></div>
-                </Col>
-                <Col xs={12} xl={8} className="mt-4">
-                    <div style={{backgroundColor: '#292929', height: '320px'}}></div>
-                </Col>
-                <Col xs={12} xl={4} className="mt-4">
-                    <div style={{backgroundColor: '#292929', height: '320px'}}></div>
-                </Col>
-                <Col xs={12} xl={4} className="mt-4">
-                    <div style={{backgroundColor: '#292929', height: '320px'}}></div>
-                </Col>
-                <Col xs={12} xl={4} className="mt-4">
-                    <div style={{backgroundColor: '#292929', height: '320px', borderRadius: '0 0 30% 0'}}></div>
-                </Col>
-            </Row>
+            {filteredProjects.map((project) => (
+                <Row key={project.id}>
+                    {project.properties.map((bestProject) => (
+
+                        <Col xs={12} xl={bestProject.id === 2 ? 8 : 4} className="mt-4" key={bestProject.id}>
+                            <div className="overlay-container" style={{ borderRadius: bestProject.id === 5 ? '0 0 30% 0' : '0' }}>
+                                <Image src={bestProject.image} style={{ width: '100%', height: '100%' }} />
+                                <div className="overlay">
+                                    <div className="overlay-content">
+                                        <Row>
+                                            <Col xs={12} xl={12} className="d-flex justify-content-end">
+                                                <Image src={ArrowExample} style={{ position: 'absolute', width: '80px', right: '0', top: '0' }} />
+                                            </Col>
+                                        </Row>
+                                        <Row style={{ marginTop: bestProject.id === 2 ? '22%' : '45%' }}>
+                                            <Col xs={12} xl={12}>
+                                                <p>{bestProject.year}</p>
+                                            </Col>
+                                            <Col xs={12} xl={12}>
+                                                <p style={{ fontSize: '36px', fontWeight: '600' }}>{bestProject.title}</p>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            ))}
         </Container>
 
     );
